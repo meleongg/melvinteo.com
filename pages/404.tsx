8813b067375d92620f4bee43a59382dccac1f57b
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2022-12-03 11:56:52 -0800

    updated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+import errorStyles from "../styles/404.module.css";
+import utilStyles from "../styles/utils.module.css";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className={`${errorStyles.container}`}>
+      <h1>404: Page Not Found</h1>
+      <h2>Sorry! This page doesn't exist!</h2>
+      <Link className={utilStyles.link} href="/">
+        Back to home
+      </Link>
+    </div>
+  );
 }
