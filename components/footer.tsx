45cdb2bee124bb047291565ef6915ee1e885f9b7
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2023-08-06 16:06:58 -0700

    updated footer copyright date

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,21 +9,36 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer className={footerStyles.footer}>
-            <p>Melvin Teo @ 2022</p>
+            <p>Melvin Teo @ 2023</p>
             <div className={footerStyles.socials}>
                 <Link href="mailto:mthteo@gmail.com" target="_blank">
-                    <FontAwesomeIcon className={footerStyles.socialIcon} icon={faEnvelope} />
+                    <FontAwesomeIcon
+                        className={footerStyles.socialIcon}
+                        icon={faEnvelope}
+                    />
                 </Link>
                 <Link href="https://github.com/meleongg" target="_blank">
-                    <FontAwesomeIcon className={footerStyles.socialIcon} icon={faGithub} />
+                    <FontAwesomeIcon
+                        className={footerStyles.socialIcon}
+                        icon={faGithub}
+                    />
                 </Link>
-                <Link href="https://www.linkedin.com/in/melvinhteo/" target="_blank">
-                    <FontAwesomeIcon className={footerStyles.socialIcon} icon={faLinkedinIn} />
+                <Link
+                    href="https://www.linkedin.com/in/melvinhteo/"
+                    target="_blank"
+                >
+                    <FontAwesomeIcon
+                        className={footerStyles.socialIcon}
+                        icon={faLinkedinIn}
+                    />
                 </Link>
                 <Link href="https://twitter.com/meleongg" target="_blank">
-                    <FontAwesomeIcon className={footerStyles.socialIcon} icon={faTwitter} />
+                    <FontAwesomeIcon
+                        className={footerStyles.socialIcon}
+                        icon={faTwitter}
+                    />
                 </Link>
             </div>
         </footer>
-    )
+    );
 }
